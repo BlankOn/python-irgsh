0264e9608057edbc1d225f209c2aad387adbe3bc
mdamt <mdamt@adikih>
2010-09-25 20:09:42 +0300

    Hack for custom repository component target as it is not easily done with pbuilder without hooks

--- irgsh/builder_pbuilder.py
+++ irgsh/builder_pbuilder.py
@@ -2,6 +2,7 @@
 
 from buildexceptions import *
 import os
+from stat import *
 import shutil
 from subprocess import Popen
 from subprocess import PIPE
@@ -15,6 +16,7 @@ class builder_pbuilder(BuilderIface):
     """
 
     _conf = None
+    _extra_component_hook = "D01extracomponents"
     def __init__(self, distro, build_directory = None, results_directory = None):
         """Creates a Builder instance
         
@@ -31,6 +33,20 @@ class builder_pbuilder(BuilderIface):
         self._conf = self._path + "/" + self._distro.name + "/pbuilder.conf" 
         if os.path.exists(self._conf) == False:
             raise BuildBuilderConfigurationError("Unable to open %s" % (self._conf))
+        self._conf_results_directory = None
+
+        self._mirror = ""
+        self._hookdir = ""
+        file = open(self._conf)
+        for line in file:
+            if line.startswith("BUILDRESULT"):
+                self._conf_results_directory = line.replace("BUILDRESULT", "").strip("=").strip().strip("\"").strip()
+            elif line.startswith("MIRRORSITE="):
+                self._mirror = line.replace("MIRRORSITE", "").strip("=").strip().strip("\"").strip()
+            elif line.startswith("HOOKDIR="):
+                self._hookdir = line.replace("HOOKDIR", "").strip("=").strip().strip("\"").strip()
+
+        file.close()
 
     """ /reimp
     """
@@ -40,8 +56,17 @@ class builder_pbuilder(BuilderIface):
         dir = os.path.dirname(source)
         os.chdir(dir)
         
+        # Clears hook file before building
+        # It will be regenerated when necessary prior building
+        # in generate_arguments()
+        if self._hookdir:
+            hookfile = os.path.join(self._hookdir, self._extra_component_hook)
+            if os.path.isfile(hookfile):
+                os.unlink(hookfile)
+
         log = BuildLog()
         args = self.generate_build_arguments(source)
+        print "Building with arguments: %s" % args
         p = Popen("sudo pbuilder " + args, shell=True, stdout=log.handle(), stderr=log.handle())
         os.waitpid(p.pid, 0)
 
@@ -71,15 +96,8 @@ class builder_pbuilder(BuilderIface):
     @property
     def results_directory(self):
         if self._results_directory == None:
-            results_directory = None
 
-            file = open(self._conf)
-            for line in file:
-                if line.startswith("BUILDRESULT"):
-                    results_directory = line.replace("BUILDRESULT", "").strip("=").strip()
-                    break
-            file.close()
-
+            results_directory = self._conf_results_directory
             if results_directory == None:
                 raise BuilderBuildConfigurationError("Results directory is not defined in %s configuration file" % (self._conf))
             return results_directory
@@ -94,6 +112,18 @@ class builder_pbuilder(BuilderIface):
 
         if self._results_directory != None:
             args += " --buildresult " + self._results_directory
+
+        if self._components != None:
+            if os.path.isdir(self._hookdir):
+                script = "#!/bin/sh\n"
+                script += "echo \"deb %s %s %s\" >> /etc/apt/sources.list\n" % (self._mirror, self._distro.name, self._components)
+                script += "apt-get update\n"
+
+                hook = os.path.join(self._hookdir, self._extra_component_hook)
+                f = open(hook, "w")
+                f.write(script)
+                f.close()
+                os.chmod(hook, S_IRUSR | S_IXUSR | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) 
 
         path = self._path + "/" + self._distro.name 
         args += " --configfile " + path + "/pbuilder.conf "
