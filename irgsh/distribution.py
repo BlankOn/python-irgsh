ca5eda333be99c1bbc8e7372f5dc7f3545e39826
Fajran Iman Rusadi <fajran@gmail.com>
2010-12-08 22:54:45 +0100

    Dist in distribution spec is a single value

--- irgsh/distribution.py
+++ irgsh/distribution.py
@@ -1,8 +1,8 @@
 class Distribution(object):
-    def __init__(self, name, mirror, dists, components=['main'], extra=[]):
+    def __init__(self, name, mirror, dist, components=['main'], extra=[]):
         self.name = name
         self.mirror = mirror
-        self.dists = dists
+        self.dist = dist
         self.components = components
         self.extra = extra
 
