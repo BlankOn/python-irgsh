ad382e98ea6ecb1d4ee0c1fd95402f049ee0b4e8
Fajran Iman Rusadi <fajran@gmail.com>
2010-12-09 06:47:45 +0100

    Include version information in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@
 
 from setuptools import setup
 
-setup(name='python-irgsh', 
-      version='__VERSION__',
+def get_version():
+    import irgsh
+    return irgsh.__version__
+
+setup(name='irgsh', 
+      version=get_version(),
       description='Python interface to irgsh',
       url='http://irgsh.blankonlinux.or.id',
       packages=['irgsh'],
       maintainer='BlankOn Developers',
       maintainer_email='blankon-dev@googlegroups.com',
      )
+
