b3822503b0a423d1e9505ad6dd4ed926c8cb5025
Fajran Iman Rusadi <fajran@gmail.com>
2010-12-19 12:41:02 +0100

    Refactor packager's build() method so it exposes internal calls.

--- irgsh/packager.py
+++ irgsh/packager.py
@@ -15,14 +15,27 @@ class Packager(object):
         self.stderr = stderr
 
     def build(self):
+        '''Build package.
+
+        This method will sequentially call export_source, retrieve_orig,
+        genereate_dsc, and build_package.
+
+        To call those four methods individually, use the following.
+
+            >>> dsc_dir = "/tmp/dsc/"
+            >>> source_dir = "/tmp/src/"
+            >>> self.export_source(source_dir)
+            >>> orig_path = self.retrieve_orig()
+            >>> dsc = self.generate_dsc(dsc_dir, source_dir, orig_path)
+            >>> dsc_path = os.path.join(dsc_dir, dsc)
+            >>> self.build_package(dsc_path)
+        '''
         try:
             target = tempfile.mkdtemp('-irgsh-builder')
-            fname = self._generate_dsc(target)
+            dsc = self._generate_dsc(target)
 
-            dsc = os.path.join(target, fname)
-            result = self.builder.build(dsc, self.resultdir,
-                                        self.stdout, self.stderr)
-
+            dsc_path = os.path.join(target, dsc)
+            result = self.build_package(dsc_path)
             return result
 
         finally:
@@ -31,16 +44,11 @@ class Packager(object):
     def _generate_dsc(self, target):
         try:
             dirname = tempfile.mkdtemp('-irgsh-builder-source')
-            source = self.specification.get_source()
-            source.export(dirname)
+            self.export_source(dirname)
 
-            orig = self.specification.orig
-            orig_path = None
-            if orig is not None:
-                (orig_path, tmp) = urllib.urlretrieve(orig)
+            orig_path = self.retrieve_orig()
 
-            pkg = SourcePackage(dirname, orig_path)
-            dsc = pkg.generate_dsc(target, self.stdout, self.stderr)
+            dsc = self.generate_dsc(target, dirname, orig_path)
 
             if orig_path is not None:
                 os.remove(orig_path)
@@ -49,6 +57,33 @@ class Packager(object):
 
         finally:
             shutil.rmtree(dirname)
+
+    def export_source(self, target):
+        '''Extract source file to the given target directory.
+        '''
+        source = self.specification.get_source()
+        source.export(target)
+
+    def retrieve_orig(self):
+        '''Download orig file, if available.
+        '''
+        orig = self.specification.orig
+        orig_path = None
+        if orig is not None:
+            orig_path, tmp = urllib.urlretrieve(orig)
+        return orig_path
+
+    def generate_dsc(self, target, source_dir, orig_path):
+        '''Generate dsc file from given source directory and path to the
+        orig file in the target directory.
+        '''
+        pkg = SourcePackage(source_dir, orig_path)
+        return pkg.generate_dsc(target)
+
+    def build_package(self, dsc_path):
+        '''Build a package given its dsc file (fullpath).
+        '''
+        return self.builder.build(dsc_path, self.resultdir, self.stdout, self.stderr)
 
 def _test_run():
     from subprocess import Popen
