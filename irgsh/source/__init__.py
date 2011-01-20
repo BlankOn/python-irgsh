7fb172c81be1f37ca145761562ce216c570a5038
Fajran Iman Rusadi <fajran@gmail.com>
2011-01-20 12:17:16 +0100

    Add function to prepare source package

--- irgsh/source/__init__.py
+++ irgsh/source/__init__.py
@@ -1,3 +1,4 @@
 from .packager import SourcePackageBuilder
 from .downloader import SourceDownloader
+from .utils import prepare_source_package
 
---
+++ irgsh/source/utils.py
@@ -0,0 +1,15 @@
+from .downloader import SourceDownloader
+from .packager import SourcePackageBuilder
+
+def prepare_source_package(target, source, source_type='tarball',
+                           source_opts=None):
+    if source_opts is None:
+        source_opts = {}
+
+    if source_type == 'dsc':
+        downloader = SourceDownloader(source, source_opts.get('base', None))
+        return downloader.download(target)
+    else:
+        packager = SourcePackageBuilder(source, source_type, source_opts)
+        return packager.build(target)
+
