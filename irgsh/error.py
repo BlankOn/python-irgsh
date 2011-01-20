7b02d656031d109244e65618be08cee28ba53d7d
Fajran Iman Rusadi <fajran@gmail.com>
2011-01-20 13:51:01 +0100

    Remove unused exceptions

--- irgsh/error.py
+++ irgsh/error.py
@@ -7,19 +7,6 @@ class BuildFailedError(IrgshException):
     def __str__(self):
         return 'Build failed: %s' % self.source
 
-class InvalidSourceLocationError(IrgshException):
-    def __init__(self, location, msg=None):
-        self.location = location
-        self.msg = msg
-
-        desc = 'Invalid source location: %s' % self.location
-        if msg is not None:
-            desc = '%s (%s)' % (desc, msg)
-        super(InvalidSourceLocationError, self).__init__(desc)
-
-class InvalidSourceNameError(IrgshException):
-    pass
-
 class UploadFailedError(IrgshException):
     def __init__(self, changes, code, log=None):
         self.changes = changes
@@ -27,7 +14,4 @@ class UploadFailedError(IrgshException):
         self.log = log
     def __str__(self):
         return 'Build failed: %s (%s)' % (self.changes, self.code)
-
-class InvalidControlFile(IrgshException):
-    pass
 
